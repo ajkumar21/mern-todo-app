df6e71edfecd6c9b32c06d50eb336342e8d2f1ef
AJ Kumar <ajanthan29894@gmail.com>
2019-06-11 19:11:26 +0100

    Connected post todo and get todo to front end

--- src/components/create-todo.jsx
+++ src/components/create-todo.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class createToDo extends Component {
   constructor(props) {
@@ -35,14 +36,28 @@ class createToDo extends Component {
 
   onSubmit(e) {
     e.preventDefault(); //default html behaviour is prevented
+
+    console.log(`Form submitted:`);
+    console.log(`Todo Description: ${this.state.todo_description}`);
+    console.log(`Todo Responsible: ${this.state.todo_responsible}`);
+    console.log(`Todo Priority: ${this.state.todo_priority}`);
+    console.log(`Todo Priority: ${this.state.todo_completed}`);
+
+    const newTodo = {
+      todo_description: this.state.todo_description,
+      todo_responsible: this.state.todo_responsible,
+      todo_priority: this.state.todo_priority,
+      todo_completed: this.state.todo_completed
+    };
+    axios
+      .post('http://localhost:4000/todos/add', newTodo)
+      .then(res => console.log(res.status + ' ' + res.data));
     this.setState({
       todo_description: '',
       todo_responsible: '',
       todo_priority: '',
       todo_completed: false
     }); //reset fields
-
-    //need to submit to backend here
   }
   render() {
     return (
--- src/components/todos-list.jsx
+++ src/components/todos-list.jsx
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Table } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 class toDo extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:4000/todos/')
+      .then(res => {
+        this.setState({ todos: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  getTable() {
+    return this.state.todos.map((todo, i) => {
+      return (
+        <tr key={i}>
+          <td>{todo.todo_description}</td>
+          <td>{todo.todo_responsible}</td>
+          <td>{todo.todo_priority}</td>
+          <td>{todo.todo_completed ? 'Yes' : 'No'}</td>
+          <td>
+            <Link to={'/edit/' + todo._id}>Edit</Link>
+          </td>
+        </tr>
+      );
+    });
+  }
+
   render() {
-    return <div>to do list</div>;
+    return (
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Owner</th>
+            <th>Priority</th>
+            <th>Completion Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{this.getTable()}</tbody>
+      </Table>
+    );
   }
 }
 
