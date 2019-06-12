b59154f40499492e85eeb500e788c45d9824f687
AJ Kumar <ajanthan29894@gmail.com>
2019-06-12 15:45:08 +0100

    Added new title and added Add button instead of having a new page. Adding details through a modal instead of a page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodosList from './components/todos-list';
 import EditTodo from './components/edit-todo';
 import CreateTodo from './components/create-todo';
+import Create from './components/create-todo';
+import { Navbar } from 'react-bootstrap';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='container'>
-        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-          <Link to='/' className='navbar-brand'>
-            MERN-Stack Todo App
-          </Link>
-          <div className='collpase navbar-collapse'>
-            <ul className='navbar-nav mr-auto'>
-              <li className='navbar-item'>
-                <Link to='/' className='nav-link'>
-                  Todos
-                </Link>
-              </li>
-              <li className='navbar-item'>
-                <Link to='/create' className='nav-link'>
-                  Create Todo
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <Navbar bg='dark' variant='dark'>
+          <Navbar.Brand href='/'>{'My to do list'}</Navbar.Brand>
+          <br />
+          <Create />
+        </Navbar>
+
         <br />
         <Switch>
           <Route path='/' exact component={TodosList} />
--- src/components/create-todo.jsx
+++ src/components/create-todo.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { ModalBody, Button, Modal, ModalHeader, ModalFooter } from 'reactstrap';
 
 class createToDo extends Component {
   constructor(props) {
@@ -8,7 +9,8 @@ class createToDo extends Component {
       todo_description: '',
       todo_responsible: '',
       todo_priority: '',
-      todo_completed: false
+      todo_completed: false,
+      modal: false
     };
 
     //As these functions are dealing with the component State object
@@ -16,8 +18,13 @@ class createToDo extends Component {
     this.onChangeTodoResponsible = this.onChangeTodoResponsible.bind(this);
     this.onChangeTodoPriority = this.onChangeTodoPriority.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
-
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
   onChangeTodoDescription(e) {
     this.setState({ todo_description: e.target.value });
   }
@@ -52,77 +59,88 @@ class createToDo extends Component {
       todo_priority: '',
       todo_completed: false
     }); //reset fields
+    this.toggle();
   }
   render() {
     return (
-      <div style={{ marginTop: 10 }}>
-        <h3>Create New Todo</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className='form-group'>
-            <label>Description: </label>
-            <input
-              type='text'
-              className='form-control'
-              value={this.state.todo_description}
-              onChange={this.onChangeTodoDescription}
-            />
-          </div>
-          <div className='form-group'>
-            <label>Responsible: </label>
-            <input
-              type='text'
-              className='form-control'
-              value={this.state.todo_responsible}
-              onChange={this.onChangeTodoResponsible}
-            />
-          </div>
-          <div className='form-group'>
-            <div className='form-check form-check-inline'>
+      <div>
+        <Button color='primary' onClick={this.toggle}>
+          Add
+        </Button>
+        <Modal
+          isOpen={this.state.modal}
+          toggle={this.toggle}
+          className={this.props.className}
+        >
+          <ModalHeader toggle={this.toggle}>Add To Do</ModalHeader>
+          <ModalBody>
+            <div className='form-group'>
+              <label>Description: </label>
               <input
-                className='form-check-input'
-                type='radio'
-                name='priorityOptions'
-                id='priorityLow'
-                value='Low'
-                checked={this.state.todo_priority === 'Low'}
-                onChange={this.onChangeTodoPriority}
+                type='text'
+                className='form-control'
+                value={this.state.todo_description}
+                onChange={this.onChangeTodoDescription}
               />
-              <label className='form-check-label'>Low</label>
             </div>
-            <div className='form-check form-check-inline'>
+            <div className='form-group'>
+              <label>Responsible: </label>
               <input
-                className='form-check-input'
-                type='radio'
-                name='priorityOptions'
-                id='priorityMedium'
-                value='Medium'
-                checked={this.state.todo_priority === 'Medium'}
-                onChange={this.onChangeTodoPriority}
+                type='text'
+                className='form-control'
+                value={this.state.todo_responsible}
+                onChange={this.onChangeTodoResponsible}
               />
-              <label className='form-check-label'>Medium</label>
             </div>
-            <div className='form-check form-check-inline'>
-              <input
-                className='form-check-input'
-                type='radio'
-                name='priorityOptions' //radio buttons with same name, only one can be selected (grouped by name)
-                id='priorityHigh'
-                value='High'
-                checked={this.state.todo_priority === 'High'}
-                onChange={this.onChangeTodoPriority}
-              />
-              <label className='form-check-label'>High</label>
+            <div className='form-group'>
+              <div className='form-check form-check-inline'>
+                <input
+                  className='form-check-input'
+                  type='radio'
+                  name='priorityOptions'
+                  id='priorityLow'
+                  value='Low'
+                  checked={this.state.todo_priority === 'Low'}
+                  onChange={this.onChangeTodoPriority}
+                />
+                <label className='form-check-label'>Low</label>
+              </div>
+              <div className='form-check form-check-inline'>
+                <input
+                  className='form-check-input'
+                  type='radio'
+                  name='priorityOptions'
+                  id='priorityMedium'
+                  value='Medium'
+                  checked={this.state.todo_priority === 'Medium'}
+                  onChange={this.onChangeTodoPriority}
+                />
+                <label className='form-check-label'>Medium</label>
+              </div>
+              <div className='form-check form-check-inline'>
+                <input
+                  className='form-check-input'
+                  type='radio'
+                  name='priorityOptions' //radio buttons with same name, only one can be selected (grouped by name)
+                  id='priorityHigh'
+                  value='High'
+                  checked={this.state.todo_priority === 'High'}
+                  onChange={this.onChangeTodoPriority}
+                />
+                <label className='form-check-label'>High</label>
+              </div>
             </div>
-          </div>
+          </ModalBody>
 
-          <div className='form-group'>
-            <input
-              type='submit'
-              value='Create Todo'
-              className='btn btn-primary'
-            />
-          </div>
-        </form>
+          <ModalFooter>
+            <Button color='primary' onClick={this.onSubmit}>
+              Save
+            </Button>{' '}
+            <Button color='secondary' onClick={this.toggle}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </Modal>
       </div>
     );
   }
